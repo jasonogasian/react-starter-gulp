7405c2afd5f36f4dd934aaf1e444310c370b37aa
Jason Ogasian <jogasian@book.com>
2015-12-10 11:20:25 -0800

    Use a config fule for gulp params.

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,9 @@ var watchify = require('watchify');					// Only update changed files
 var reactify = require('reactify');					// JSX -> JS transformation
 var streamify = require('gulp-streamify');
 var clean = require('gulp-clean');
+var config = require('./config');
 
 
-// Source and destination path definitions for gulp tasks
-var path = {
-  HTML: 'src/index.html',
-  MINIFIED_OUT: 'main.min.js',
-  OUT: 'main.js',
-  RELEASE: 'release',
-  DEV: 'build',
-  DEST_DEV: 'src/',
-  DEST_RELEASE: 'scripts/',
-  ENTRY_POINT: './src/js/App.js'
-};
 
 
 //
@@ -29,20 +19,20 @@ gulp.task('default', ['watch']);
 
 // Copy HTML to DEST and point to the compiled JS in the build directory
 gulp.task('replaceHTMLsrc', function(){
-  gulp.src(path.HTML)
+  gulp.src(config.HTML)
     .pipe(htmlreplace({
-      'js': path.DEST_DEV + path.OUT
+      'js': config.DEST_DEV + config.OUT
     }))
-    .pipe(gulp.dest(path.DEV));
+    .pipe(gulp.dest(config.DEV));
 });
 
 // Transform JSX and bundle on file changes
 gulp.task('watch', ['replaceHTMLsrc'], function() {
-  gulp.watch(path.HTML, ['replaceHTMLsrc']);
+  gulp.watch(config.HTML, ['replaceHTMLsrc']);
 
   // Use watchify with browserify so that only changed files are updated. FASTER
   var watcher  = watchify(browserify({
-    entries: [path.ENTRY_POINT],  // Browserify will traverse to sub-files!
+    entries: [config.ENTRY_POINT],  // Browserify will traverse to sub-files!
     transform: [reactify],				// JSX->JS
     debug: true,									// User source maps for transformed code (JSX)
     cache: {}, packageCache: {}, fullPaths: true // Required junk (ignore)
@@ -51,14 +41,14 @@ gulp.task('watch', ['replaceHTMLsrc'], function() {
     console.log('Initial Update');
   return watcher.on('update', function () {
     watcher.bundle()	// Concat JS into one file and resolve the requires
-      .pipe(source(path.OUT))
-      .pipe(gulp.dest(path.DEV + '/' + path.DEST_DEV))
+      .pipe(source(config.OUT))
+      .pipe(gulp.dest(config.DEV + '/' + config.DEST_DEV))
       console.log('Updated');
   })
   	// Execute the first time without an update
     .bundle()
-    .pipe(source(path.OUT))
-    .pipe(gulp.dest(path.DEV + '/' + path.DEST_DEV));
+    .pipe(source(config.OUT))
+    .pipe(gulp.dest(config.DEV + '/' + config.DEST_DEV));
 });
 
 
@@ -69,22 +59,22 @@ gulp.task('release', ['replaceHTMLmin', 'build']);
 
 // Copy HTML to DEST and point to the compiled JS in the build directory
 gulp.task('replaceHTMLmin', function(){
-  gulp.src(path.HTML)
+  gulp.src(config.HTML)
     .pipe(htmlreplace({
-      'js': path.DEST_RELEASE + path.MINIFIED_OUT
+      'js': config.DEST_RELEASE + config.MINIFIED_OUT
     }))
-    .pipe(gulp.dest(path.RELEASE));
+    .pipe(gulp.dest(config.RELEASE));
 });
 
 gulp.task('build', function(){
   browserify({
-    entries: [path.ENTRY_POINT],
+    entries: [config.ENTRY_POINT],
     transform: [reactify]
   })
     .bundle()
-    .pipe(source(path.MINIFIED_OUT))
+    .pipe(source(config.MINIFIED_OUT))
     .pipe(streamify(uglify()))
-    .pipe(gulp.dest(path.RELEASE + '/' + path.DEST_RELEASE));
+    .pipe(gulp.dest(config.RELEASE + '/' + config.DEST_RELEASE));
 });
 
 
@@ -92,6 +82,6 @@ gulp.task('build', function(){
 // Cleanup
 //
 gulp.task('clean', function () {
-	return gulp.src(path.DEV, {read: false})
+	return gulp.src(config.DEV, {read: false})
 		.pipe(clean());
 });
